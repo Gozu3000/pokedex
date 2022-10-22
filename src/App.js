47c2130c63bc0091ff28934657a9d0ceb6596692
Gozu3000 <erick.sanchezcorrea@gmail.com>
2022-10-21 21:21:43 -0500

    5to commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   const [pokemon, setPokemon] = useState( {} )
   const [busqueda, setbusqueda] = useState(null)
   
+  let variable 
+
   function HandleChange () {
     setbusqueda(document.getElementById('search').value)
-      // busqueda && mostrarPokemons(busqueda)
-      console.log(busqueda)
   }
 
 
@@ -19,8 +19,6 @@ function App() {
       busqueda && mostrarPokemons(busqueda)
       console.log(busqueda)
     }
-
-    
 
   const mostrarPokemons = (nombre='arcanine')=>{
     let pokemon = nombre.toLowerCase().trim()
@@ -43,9 +41,6 @@ function App() {
       mostrarPokemons()
   }, [] )
 
-  // console.log(busqueda)
-  // console.log(pokemon)
-
   return (
     <div className="App">
         <h1>Pokédex</h1>
@@ -55,13 +50,16 @@ function App() {
           <div className='pokedex-front'>
             <div className='img-container'>
               <img src={ pokemon.avatar_front || 'https://i.pinimg.com/736x/9d/dd/1f/9ddd1f367e36919648741d77c86e75d0.jpg'} alt={pokemon.name} />
-              <input type="text"  placeholder='...Busca un pokemon' autoComplete='off' name='search'  id='search' />
-              <input type="button" value='Buscar' onClick={HandleClick} onChange={HandleChange}/>
+              <input type="text"  placeholder='...Busca un pokemon' autoComplete='off' name='search'  id='search' onChange={HandleChange} />
+              <input type="button" value='Buscar' onClick={HandleClick}/>
             </div>
           </div>
 
           <div className='pokedex-back'> 
               <table>
+                <tr>
+                  <th colSpan={2} style={{height:'20px',textAlign:'center',textTransform:'capitalize'}}>{pokemon.name}</th>
+                </tr>
                 <tbody>
                 {
                   Object.entries(pokemon).length > 1 && pokemon.stats.map((el, index)=>(
